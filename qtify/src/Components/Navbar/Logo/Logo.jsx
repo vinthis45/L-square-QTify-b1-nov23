957a00e52ec9cfe066fadc20a88bba94359802d2
Abinash <asavisingh72@gmail.com>
2023-11-14 14:06:38 +0530

    second commit

--- qtify/src/Components/Navbar/Logo/Logo.jsx
+++ qtify/src/Components/Navbar/Logo/Logo.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import mainLogo from "../../../Assets/Logo.png";
 export default function Logo() {
     return (
-        <img src={mainLogo} alt="Logo" width={67} />
+        <div style={{display: 'flex', margin: '14px'}}>
+            <img src={mainLogo} alt="Logo" width={67} />
+        </div>
     )
 }
